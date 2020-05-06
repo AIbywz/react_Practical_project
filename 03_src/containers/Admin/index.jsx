20f2256d472106af18eec3a4b5698b5f6d3e2fb9
bai yan <3410383734@qq.com>
2020-05-06 19:37:47 +0800

    updata

---
+++ 03_src/containers/Admin/index.jsx
@@ -0,0 +1,37 @@
+
+
+import React, { Component } from 'react'
+//redux
+import { connect } from 'react-redux'
+
+//antd 
+import { Layout } from "antd";
+//自定义
+import './admin.less'
+import Header from './Header/Header'
+import check from '@/containers/HOC/check'
+
+const { Footer, Sider, Content } = Layout;
+
+@connect(
+  (state)=>({isLogin:state.userInfo.isLogin}),
+  {}
+)
+@check
+class Admin extends Component {
+  render() {
+    //没有登录时
+    return (
+      <Layout className="layout-style">
+        <Sider>Sider</Sider>
+        <Layout>
+          <Header />
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+    </Layout>
+    )
+  }
+}
+
+export default Admin
---
+++ 03_src/redux/actions_type.js
@@ -0,0 +1,4 @@
+//定义 动作 常量
+export const SAVE_USERINFO = 'save_userinfo'  //保存用户信息
+
+export const DELETE_USERINFO = 'delete_userinfo' //删除用户信息
